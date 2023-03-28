e998b830aee6c59cebc7fd7cae5f57b3a0f7714e
Adachima <sapondanaisriwan@gmail.com>
2023-03-29 01:18:23 +0700

    Update: main.py

--- main.py
+++ main.py
@@ -4,35 +4,27 @@ import os
 import zipfile
 
 
-def duplicate_excel_file(original_file, output_file):
+def duplicate_file(original_file, output_file):
     shutil.copyfile(original_file, output_file)
 
 
+def file_exists(file_path):
+    return os.path.isfile(file_path)
+
+
+def check_input_file(original_file):
+    if not file_exists(original_file):
+        return print("The file does not exist. Please rename to input.xlsx")
+
+
 def check_zip_file(original_file, zip_file_path):
-    if not os.path.exists(zip_file_path):
-        print("File does not exist. Creating a new file.")
-        duplicate_excel_file(original_file, zip_file_path)
-
-    with zipfile.ZipFile(zip_file_path, 'r') as archive:
-        if not archive.namelist():
-            print("Zip file is empty. Creating a new file.")
-            duplicate_excel_file(original_file, zip_file_path)
+    if not file_exists(zip_file_path):
+        duplicate_file(original_file, zip_file_path)
+        return print("Create input.zip")
 
 
-def remove_file(file_path):
-    if os.path.exists(file_path):
-        os.remove(file_path)
-
-
-def rename_file(file_path):
-    if os.path.exists(file_path):
-        file_name, file_extension = os.path.splitext(file_path)
-        os.rename(file_path, file_name + ".xlsx")
-
-
-def modify_worksheets(zip_file_path, file_path, regex_pattern):
-    new_zip_file_path = "output.zip"
-    with zipfile.ZipFile(new_zip_file_path, mode="w") as new_zip_file:
+def modify_worksheets(output_file, zip_file_path, file_path, regex_pattern):
+    with zipfile.ZipFile(output_file, mode="w") as new_zip_file:
         with zipfile.ZipFile(zip_file_path, mode="r") as zip_file:
             for file_info in zip_file.infolist():
                 if file_info.filename.startswith(file_path) and file_info.filename.endswith(".xml"):
@@ -43,20 +35,32 @@ def modify_worksheets(zip_file_path, file_path, regex_pattern):
                     new_zip_file.writestr(
                         file_info.filename, zip_file.read(file_info))
 
-    return new_zip_file_path
+    return output_file
+
+
+def remove_file(file_path):
+    if file_exists(file_path):
+        os.remove(file_path)
+        return print("Remove zip file")
 
 
 def main():
     original_file = "./input.xlsx"
+    output_file = "output.xlsx"
     zip_file_path = "./input.zip"
     file_path = "xl/worksheets/"
     regex_pattern = re.compile(r"<sheetProtection(.+?)/>")
 
+    print("Checking...")
+    check_input_file(original_file)
     check_zip_file(original_file, zip_file_path)
-    output_file_path = modify_worksheets(
-        zip_file_path, file_path, regex_pattern)
-    rename_file(output_file_path)
+
+    print("Create output.xlsx")
+    modify_worksheets(output_file,
+                      zip_file_path, file_path, regex_pattern)
+
     remove_file(zip_file_path)
+    print("Done")
 
 
 if __name__ == '__main__':
